2ad925d968bd8f196f2d9a9abba941ddf2301a18
arthnangia <48117774+arthnangia@users.noreply.github.com>
2019-08-24 20:07:50 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-
-# Calculator
-# Have the user enter 2 number - a. b -
-# and an operator - op - and calculate
-# the solution - c - according to the
-# type of the given operator
+# Basis Calculator
+# Accept 2 numbers from user - x and y -
+# and an operator - op - and calculate the solution - z - according to the
+# type of the given operator.
 
 
 def calc(a, b, op):
